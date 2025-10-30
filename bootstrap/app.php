bd4526222be455fe065415ba1345ea346936bd61
jquispedelgado667 <jquispedelgado667@gmail.com>
2025-10-30 10:18:15 -0400

    Register view support in bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+    ->withView()
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'role' => CheckRole::class, 
